49e59da9a4b25b8966bb222dd07ed5f5a2088cb6
Joanna <ulanowska.joanna@gmail.com>
2026-03-31 21:15:03 +0200

    return success bool fix

--- url-monitoring.py
+++ url-monitoring.py
@@ -44,7 +44,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'success': status,
+            'success': success,
             'message': message
         })
     }

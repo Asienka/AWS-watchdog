6fc487182f2c76949141bbe587906494c3699e46
Joanna <ulanowska.joanna@gmail.com>
2026-05-02 11:24:59 +0200

    Refactor ECR lifecycle policy and add SNS topic with subscriptions for lambda alerts

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,27 +6,17 @@ resource "aws_ecr_repository" "url_monitoring_repository" {
   }
 }
 
-resource "aws_sns_topic" "url_monitor_alerts" {
-  name = "url-monitor-alerts"
-}
-resource "aws_sns_topic_subscription" "email_alert" {
-  topic_arn = aws_sns_topic.url_monitor_alerts.arn
-  protocol  = "email"
-  endpoint  = var.sns_alert_email
+  resource "aws_ecr_lifecycle_policy" "main_policy" {
+    repository = aws_ecr_repository.url_monitoring_repository.name
 
-}
-
-resource "aws_ecr_lifecycle_policy" "main_policy" {
-  repository = aws_ecr_repository.url_monitoring_repository.name
-
-  policy = jsonencode({
+    policy = jsonencode({
     rules = [{
       rulePriority = 1
       description  = "Keep only the 5 most recent images to save space"
       selection = {
-        tagStatus   = "any"
-        countType   = "imageCountMoreThan"
-        countNumber = 5
+        tagStatus     = "any"
+        countType     = "imageCountMoreThan"
+        countNumber   = 5
       }
       action = {
         type = "expire"
@@ -34,6 +24,4 @@ resource "aws_ecr_lifecycle_policy" "main_policy" {
     }]
   })
 }
-
-
 
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -22,3 +22,10 @@ resource "aws_lambda_alias" "live" {
   function_version = aws_lambda_function.url_monitor.version
 
 }
+resource "aws_lambda_permission" "sns_invoke" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.url_monitor.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.url_monitor_alerts.arn
+}

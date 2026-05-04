a613521dfe0a280f9ad3d55f208e49c2e0d5f37a
Joanna <ulanowska.joanna@gmail.com>
2026-05-04 20:24:19 +0200

    Added self-healer Lambda function and sns permissions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -29,3 +29,30 @@ resource "aws_lambda_permission" "sns_invoke" {
   principal     = "sns.amazonaws.com"
   source_arn    = aws_sns_topic.url_monitor_alerts.arn
 }
+
+data "archive_file" "self_healer_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../self_healer.py"
+  output_path = "${path.module}/self_healer_function.zip"
+}
+resource "aws_lambda_function" "self_healer" {
+  function_name = "url-monitor-self-healer"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "self_healer.lambda_handler"
+  runtime       = "python3.11"
+  source_code_hash = data.archive_file.self_healer_zip.output_base64sha256
+
+  environment {
+    variables = {
+      LAMBDA_FUNCTION_NAME = aws_lambda_function.url_monitor.function_name
+    }
+  }
+  
+}
+resource "aws_lambda_permission" "sns_invoke_self_healer" {
+  statement_id  = "AllowExecutionFromSNSSelfHealer"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.self_healer.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.url_monitor_alerts.arn
+}
